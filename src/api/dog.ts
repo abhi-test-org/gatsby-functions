ae2d2a6ce797971b3c90dabaf24049ba6fbf27ac
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-12-23 14:58:21 -0800

    dog stuff

---
+++ src/api/dog.ts
@@ -0,0 +1,7 @@
+import fetch from "node-fetch"
+
+export default async (req, res) => {
+  const dogImage = await fetch("https://dog.ceo/api/breeds/image/random")
+  const dogJson = await dogImage.json()
+  res.status(200).send(dogJson)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react"
 
 export default function Home() {
   const [time, setTime] = useState(``)
+  const [dog, setDog] = useState(``)
 
   useEffect(() => {
     const interval = setInterval(async () => {
       const { data } = await fetch(`/functions/time.js`).then(res => res.json())
       setTime(new Date(data).toLocaleTimeString("en-US"))
     }, 1000)
+
+    const { data: dogData } = await fetch(`/functions/dog.js`).then(res => res.json())
+
+    setDog(dogData)
 
     return () => {
       clearInterval(interval)
@@ -17,6 +22,7 @@ export default function Home() {
   return (
     <div>
       <h1>The time right now is {time}</h1>
+      <img src={dog?.messagee} />
     </div>
   )
 }
