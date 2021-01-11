b82e86aea62dad3f6b7e1b283f2693cf079a8ef8
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-01-11 12:46:12 -0800

    functions logging

--- src/functions/dog.ts
+++ src/functions/dog.ts
@@ -1,6 +1,5 @@
-import fetch from "node-fetch"
-
-export default async (req, res) => {
+module.exports = async (req, res) => {
+  const fetch = require("node-fetch")
   const dogImage = await fetch("https://dog.ceo/api/breeds/image/random")
   const dogJson = await dogImage.json()
   res.status(200).send(dogJson)
