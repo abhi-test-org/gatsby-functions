f5176359cc0d2dc9268b91bb544dd12a2b425558
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-02-19 10:47:12 -0800

    tag update

--- src/functions/submit.ts
+++ src/functions/submit.ts
@@ -6,10 +6,12 @@ const client = require("twilio")(accountSid, authToken)
 export default async (req, res) => {
   const body = req.body
 
+  console.log(body)
+
   const message = await client.messages.create({
     body: body.message,
     from: "+16513582594",
-    to: body.to,
+    to: `+1${body.to}`,
   })
 
   res.status(200).send({
