d08918b79706c6e18ea8dedf0a5f4cc129784d05
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-02-19 10:35:04 -0800

    tag update

--- src/functions/dog.ts
+++ src/functions/dog.ts
@@ -1,7 +1,14 @@
 const nodeFetch = require("node-fetch")
 
 module.exports = async (req, res) => {
-  const dogImage = await nodeFetch("https://dog.ceo/api/breeds/image/random")
+  const dogImage = await nodeFetch.default(
+    "https://dog.ceo/api/breeds/image/random"
+  )
+
+  console.log(dogImage)
+
   const dogJson = await dogImage.json()
+
+  console.log(dogJson)
   res.status(200).send(dogJson)
 }
